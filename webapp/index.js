01a5484909757afe74e9b4db615b794d88767934
José Miguel Alvarez <j.m.alvarez.camacho@gmail.com>
2025-08-18 20:11:55 -0400

    agregado lista, editar

--- webapp/index.js
+++ webapp/index.js
@@ -23,6 +23,45 @@ app.get("/prueba", (req, res) => {
     const nombres = ["Juan", "Pedro", "Maria"];
     res.render("prueba", { listaNombres: nombres })
 })
+app.get('/personas', async (req, res) => {
+    const personaArr = await db.persona.findAll();
+    res.render("personas/list", { personaArr });
+})
+app.get('/personas/create', (req, res) => {
+
+    res.render("personas/form", { persona: null });
+})
+app.post('/personas/create', async (req, res) => {
+    const { nombre, apellido, edad, ciudad, fechaNacimiento } = req.body;
+    await db.persona.create({
+        nombre,
+        apellido,
+        edad,
+        ciudad,
+        fechaNacimiento
+    });
+    res.redirect('/personas');
+})
+
+app.get('/personas/:id', async (req, res) => {
+    const persona = await db.persona.findByPk(req.params.id);
+    res.render("personas/form", { persona });
+})
+app.post('/personas/:id', async (req, res) => {
+    const id = req.params.id;
+    const { nombre, apellido, edad, ciudad, fechaNacimiento } = req.body;
+    await db.persona.update({
+        nombre,
+        apellido,
+        edad,
+        ciudad,
+        fechaNacimiento
+    }, {
+        where: { id }
+    });
+    res.redirect('/personas');
+});
+
 app.get('/hola', async (req, res) => {
     res.send('¡Hola Mundo!')
     await db.persona.create({
